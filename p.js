e706cc10b7ca5851eacd329c64b222187019ca8b
Shipra Rode <150356763+ship1220@users.noreply.github.com>
2025-02-12 10:37:20 +0530

    Update p.js

--- p.js
+++ p.js
@@ -1,7 +1,10 @@
 const facts = [
-    "A 19-year-old girl with a passion for painting and art, driven by an adventurous spirit.",
-    "A cat lover whose affection for felines often inspires her artistic creations.",
-    "An adventure seeker who constantly explores new challenges and experiences through art and life."
+    "She loves to paint.",
+    "She's driven by an adventurous spirit.",
+    "She's a cat lover whose affection for felines often inspires her artistic creations.",
+    "She's an adventure seeker who constantly explores new challenges and experiences.",
+    "She loves visiting museums alone"
+    "She has made 20 mandala pieces in a day"
 ];
 
 function generateFact() {
