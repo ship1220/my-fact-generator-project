a1fdbda9fa49ca9eb15f9082f5441fa767b5340c
Shipra Rode <150356763+ship1220@users.noreply.github.com>
2025-02-12 10:48:28 +0530

    Update p.js

--- p.js
+++ p.js
@@ -4,7 +4,7 @@ const facts = [
     "She's a cat lover whose affection for felines often inspires her artistic creations.",
     "She's an adventure seeker who constantly explores new challenges and experiences.",
     "She loves visiting museums alone",
-    "She has made 20 mandala pieces in a day",
+    "She has made 20 mandala pieces in a day"
 ];
 
 function generateFact() {
